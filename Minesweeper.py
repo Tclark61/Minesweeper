c41cf6cd6a56e9284530e8dd3e5245a55695b61f
Tyler J Clark <tclark61@knights.ucf.edu>
2019-05-03 19:36:46 -0400

    First attempt at while loop

--- Minesweeper.py
+++ Minesweeper.py
@@ -9,8 +9,26 @@ template = pyautogui.locateOnScreen('template.png')
 
 print (template)
 
-small = cv2.resize(image, (0,0), fx=0.5, fy=0.5, interpolation = cv2.INTER_LANCZOS4)
+scalex = 0.1
+scaley = 0.1
+small = cv2.resize(image, None, fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
 cv2.imwrite('resizetemplate.png', small)
 newtemplate = pyautogui.locateOnScreen('resizetemplate.png')
-
+if(print(newtemplate) == "None"):
+    print("No minesweeper template found")
+    exit()
 print (newtemplate)
+
+
+
+while print(newtemplate) != None:
+    small = cv2.resize(image, (0,0), fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
+    cv2.imwrite('resizetemplate.png', small)
+    newtemplate = pyautogui.locateOnScreen('resizetemplate.png')
+    scalex = scalex + 0.1
+    scaley = scaley + 0.1
+
+scalex = scalex - 0.1
+scaley = scaley - 0.1
+print("Scale: " + str(scalex))
+    

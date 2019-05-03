b4a426bc6b6a19f1e860ba4ca9fae13f69e88cbc
JaredSJackson <jackson.s.jared3@gmail.com>
2019-05-03 19:08:57 -0400

    Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -2,15 +2,15 @@ import numpy as np
 import pyautogui
 import cv2
 
-image = cv2.imread('square.png', 0)
+image = cv2.imread('template.png', 0)
 screen = pyautogui.screenshot()
 screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
-square = pyautogui.locateOnScreen('square.png')
+template = pyautogui.locateOnScreen('template.png')
 
-print (square)
+print (template)
 
-small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
-cv2.imwrite('resizeSquare.png', small)
-newSquare = pyautogui.locateOnScreen('resizeSquare.png')
+small = cv2.resize(image, (0,0), fx=0.5, fy=0.5, interpolation = cv2.INTER_LANCZOS4)
+cv2.imwrite('resizetemplate.png', small)
+newtemplate = pyautogui.locateOnScreen('resizetemplate.png')
 
-print (newSquare)
+print (newtemplate)

46af2a490cac08b30e66f84a7aae0fa6db72a294
JaredSJackson <jackson.s.jared3@gmail.com>
2019-05-03 17:46:48 -0400

    Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,4 +1,8 @@
-print ("hello world")
-x = 5
-y = "hello"
-print (y)
+import cv2
+import numpy as np
+import pyautogui
+import cv2
+image = pyautogui.screenshot()
+image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+square = pyautogui.locateOnScreen('square.png')
+print (square)

508c80a7fcf139f486eeb8608119c45066a5380d
Tyler J Clark <tclark61@knights.ucf.edu>
2019-05-03 18:33:26 -0400

    Messing around with resize

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,8 +1,15 @@
-import cv2
 import numpy as np
 import pyautogui
 import cv2
-image = pyautogui.screenshot()
-image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+image = cv2.imread('square.png', 0)
+screen = pyautogui.screenshot()
+screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
 square = pyautogui.locateOnScreen('square.png')
+
 print (square)
+
+small = cv2.resize(image, (0,0), fx=0.5, fy=0.5)
+newSquare = pyautogui.locateOnScreen(small)
+
+print (newSquare)

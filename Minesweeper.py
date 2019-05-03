c7a641b243763e84aac8fe5fb3f5dc8c923e54ba
JaredSJackson <jackson.s.jared3@gmail.com>
2019-05-03 19:49:41 -0400

    Minesweeper.py

--- Minesweeper.py
+++ Minesweeper.py
@@ -7,8 +7,6 @@ screen = pyautogui.screenshot()
 screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
 template = pyautogui.locateOnScreen('template.png')
 
-print (template)
-
 scalex = 0.1
 scaley = 0.1
 small = cv2.resize(image, None, fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
@@ -21,10 +19,10 @@ print (newtemplate)
 
 
 
-while print(newtemplate) != None:
+while print(newtemplate) == None:
     small = cv2.resize(image, (0,0), fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
     cv2.imwrite('resizetemplate.png', small)
-    newtemplate = pyautogui.locateOnScreen('resizetemplate.png')
+    newtemplate = pyautogui.locateOnScreen('resizetemplate.png', confidence = .85)
     scalex = scalex + 0.1
     scaley = scaley + 0.1
 

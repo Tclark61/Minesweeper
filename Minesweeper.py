312c73b85e19cc22ccef62f9d5c602d5f24e47c9
Tyler J Clark <tclark61@knights.ucf.edu>
2019-05-03 22:16:02 -0400

    Officially leaving research phase, formatting code

--- Minesweeper.py
+++ Minesweeper.py
@@ -2,27 +2,37 @@ import numpy as np
 import pyautogui
 import cv2
 
-image = cv2.imread('template.png', 0)
-screen = pyautogui.screenshot()
-screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
-template = pyautogui.locateOnScreen('template.png')
+CONFIDENCE = 0.85
 
-scalex = 0.5
-scaley = 0.5
-small = cv2.resize(image, None, fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
-cv2.imwrite('resizetemplate.png', small)
+def findCoordinates(imageLoc):
+    image = cv2.imread(imageLoc, 0)
+    screen = pyautogui.screenshot()
+    screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
+    return pyautogui.locateOnScreen(imageLoc, confidence = CONFIDENCE)
 
-while(True):
-    small = cv2.resize(image, (0,0), fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
-    cv2.imwrite('resizetemplate.png', small)
-    newtemplate = pyautogui.locateOnScreen('resizetemplate.png', confidence = .85)
-    if newtemplate != None:
-        break
-    scalex = scalex + 0.1
-    scaley = scaley + 0.1
-    if(scalex > 5):
-        print("No minesweeper template found")
-        exit()
+def determineScale(imageLoc):
     
-print("Final Scale: " + str(scalex))
+    scale = 0.5
+    image = cv2.imread(imageLoc, 0)
+    scaledImage = cv2.resize(image, None, fx=scale, fy=scale, interpolation = cv2.INTER_LANCZOS4)
+    cv2.imwrite('resizetemplate.png', scaledImage)
+    while(True):
+        scaledImage = cv2.resize(image, (0,0), fx=scale, fy=scale, interpolation = cv2.INTER_LANCZOS4)
+        cv2.imwrite('resizetemplate.png', scaledImage)
+        newtemplate = pyautogui.locateOnScreen('resizetemplate.png', confidence = CONFIDENCE)
+        if newtemplate != None:
+            break
+        scale = scale + 0.05
+        if(scale > 5):
+            print("No minesweeper template found")
+            exit()
     
+    print("Final Scale: " + str(scale))
+    return scale
+    
+def main():
+    template = findCoordinates('template.png')
+    scale = determineScale('template.png')
+
+if __name__ == '__main__':
+    main()

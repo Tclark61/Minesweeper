f31dd9b9fd6c7cad96b36f3c16b0cf7335cea9cf
JaredSJackson <jackson.s.jared3@gmail.com>
2019-05-03 22:40:51 -0400

    Adding things to corner and new png files

--- Minesweeper.py
+++ Minesweeper.py
@@ -4,11 +4,15 @@ import cv2
 
 CONFIDENCE = 0.85
 
-def findCoordinates(imageLoc):
+def findCoordinates(imageLoc, scale):
+    if scale == None:
+        scale = 1
     image = cv2.imread(imageLoc, 0)
+    scaledImage = cv2.resize(image, None, fx = scale, fy = scale, interpolation = cv2.INTER_LANCZOS4)
+    cv2.imwrite('scaledImage.png', scaledImage)
     screen = pyautogui.screenshot()
     screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
-    return pyautogui.locateOnScreen(imageLoc, confidence = CONFIDENCE)
+    return pyautogui.locateOnScreen('scaledImage.png', confidence = CONFIDENCE)
 
 def determineScale(imageLoc):
     
@@ -31,8 +35,28 @@ def determineScale(imageLoc):
     return scale
     
 def main():
-    template = findCoordinates('template.png')
+    template = findCoordinates('template.png', None)
     scale = determineScale('template.png')
+    #Loading Corner and resizing it to the correct size
+    cornergui = findCoordinates('corner.png', scale)
+    print(cornergui)
+    cornerscale = determineScale('corner.png')
+    cornergui = findCoordinates('corner.png', cornerscale)
+    print(cornergui)
+
+    #Find thickness of border & bottomrightmost coordinate
+    #borderthickness = cornergui[2] - newtemplate[2]
+    #botrightcoord = (cornergui[0] + newtemplate[2], cornergui[1] + newtemplate[3])
+
+
+    #Calculate the dimensions of the board without the border
+    #topleftcoord = (newtemplate[0], newtemplate[1])
+   # botleftcoord = (newtemplate[0], botrightcoord[1])
+    #toprightcoord = (botrightcoord[0], newtemplate[1])
+    #print(topleftcoord)
+    #print(toprightcoord)
+    #print(botleftcoord)
+    #print(botrightcoord)
 
 if __name__ == '__main__':
     main()

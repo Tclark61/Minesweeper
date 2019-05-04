4af150c5e64eced26164118e9e91aae1ab293ef6
Tyler J Clark <tclark61@knights.ucf.edu>
2019-05-03 20:06:20 -0400

    Fixed, working on template choice

--- Minesweeper.py
+++ Minesweeper.py
@@ -7,21 +7,22 @@ screen = pyautogui.screenshot()
 screen = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
 template = pyautogui.locateOnScreen('template.png')
 
-scalex = 0.1
-scaley = 0.1
+scalex = 0.2
+scaley = 0.2
 small = cv2.resize(image, None, fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
 cv2.imwrite('resizetemplate.png', small)
 
 while(True):
-    print("Scale: " + str(scalex))
     small = cv2.resize(image, (0,0), fx=scalex, fy=scaley, interpolation = cv2.INTER_LANCZOS4)
     cv2.imwrite('resizetemplate.png', small)
     newtemplate = pyautogui.locateOnScreen('resizetemplate.png', confidence = .85)
-    print(newtemplate)
     if newtemplate != None:
         break
     scalex = scalex + 0.1
     scaley = scaley + 0.1
+    if(scalex > 5):
+        print("No minesweeper template found")
+        exit()
     
 print("Final Scale: " + str(scalex))
     

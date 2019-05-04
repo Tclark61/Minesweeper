bed725bb9d463651cd9b817a8ad441a3df2163f9
JaredSJackson <jackson.s.jared3@gmail.com>
2019-05-04 15:05:38 -0400

    We now have the coordinates for each box

--- Minesweeper.py
+++ Minesweeper.py
@@ -7,10 +7,8 @@ CONFIDENCE = 0.85
 
 #Every box in minesweeper is a Node
 class Node:
-   
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.coords = (x,y)
 
 
 def findCoordinates(imageLoc, scale):
@@ -61,25 +59,19 @@ def main():
     topleftcoord = (box[0], box[1])
     botleftcoord = (box[0], botrightcoord[1])
     toprightcoord = (botrightcoord[0], box[1])
-    print(topleftcoord)
-    print(toprightcoord)
-    print(botleftcoord)
-    print(botrightcoord)
 
     totalWidth = botrightcoord[0] - botleftcoord[0]
     totalHeight = botrightcoord[1] - toprightcoord[1]
     numboxeswide = int(round((totalWidth-box[2])/box[2] + 1))
     numboxestall = int(round((totalHeight-box[3])/box[3] + 1))
     numboxes = numboxeswide*numboxestall
-    print(numboxes)
     xCoord = box[0]
     yCoord = box[1]
-    board = [numboxes]
+    board = []
     for i in range(0,numboxes):
         xCoord = box[0] + (box[2]*i%numboxeswide)
         yCoord = box[1] + (box[3]*math.trunc(i/numboxestall))
-        board[i] = Node(xCoord, yCoord)
-        
+        board.append(Node(xCoord, yCoord))
 
 if __name__ == '__main__':
     main()

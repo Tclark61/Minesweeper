b6c844f7ff36231845ff75b4409785b70de6933b
Tyler J Clark <tclark61@knights.ucf.edu>
2019-05-05 23:44:44 -0400

    Working on a few important functions

--- Minesweeper.py
+++ Minesweeper.py
@@ -13,7 +13,34 @@ class Node:
         self.coords = (x,y)
         self.tuple = (x,y,width,height)
 
+def probabilityChange(probabilityDelta, box):
+    box.probability = box.probability + probabilityDelta
+    if (box.probability > 1):
+        box.probability = 1
+    if (box.probability < 0):
+        box.probability = 0
+        
+def changeNeighbors(board, clickIndex, boardInfo):
+    #Check to see how many neighbors the clicked node has
+    
+    #Set a constant probabilityDelta for now
+    probabilityDelta = 0.4
+    #Check for neighbor behind
+    if(clickIndex > 0):
+        if(board[clickIndex - 1].isClicked == False):
+            probabilityChange(probabilityDelta, board[clickIndex - 1])
+    #Check for neighbor ahead
+    if(clickIndex < (boardInfo[0] - 1)):
+        if(board[clickIndex - 1].isClicked == False):
+            probabilityChange(probabilityDelta, board[clickIndex + 1])
+    #Check for neighbor above
+    if(clickIndex >= boardInfo[1]):
+        if(board[clickIndex - boardInfo[1]].isClicked == False):
+            probabilityChange(probabilityDelta, board[clickIndex - boardInfo[1]])
+    
 
+
+    
 def findCoordinates(imageLoc, scale, color):
     if scale == None:
         scale = 1
@@ -23,7 +50,8 @@ def findCoordinates(imageLoc, scale, color):
     return pyautogui.locateOnScreen('scaledImage.png', confidence = CONFIDENCE)
 
 def determineScale(imageLoc, color):
-    
+    #Experimenting with being less safe, changed delta from 0.05 to 0.1 to half max computation time
+    delta = 0.1
     scale = 5
     image = cv2.imread(imageLoc, 0)
     while(True):
@@ -31,24 +59,29 @@ def determineScale(imageLoc, color):
         newtemplate = findCoordinates(imageLoc, scale, color)
         if newtemplate != None:
             break
-        scale = scale - 0.05
+        scale = scale - delta
         if(scale < 0.5):
             print("No minesweeper template found")
             exit()
     
     return scale
 
-def playGame(board, scale, numboxes):
+def playGame(board, scale, boardInfo):
     lowestProb = 100
     lowestProbIndex = -1
     while(findCoordinates('oFace.png', scale, 1) != None or findCoordinates('happyFace.png', scale, 1) != None):
-        for i in range(0, numboxes):
+        for i in range(0, boardInfo[0]):
+            if(board[i].probability == 1 and board[i].isClicked == False):
+                print("Found a bomb at " + str(i) + ", box is unclicked: " + str(board[i].isClicked))
+                pyautogui.click(pyautogui.center(board[lowestProbIndex].tuple), button='right')
+                board[i].isClicked = True
             if board[i].probability < lowestProb and board[i].isClicked == False:
                 lowestProb = board[i].probability
                 lowestProbIndex = i
         pyautogui.click(pyautogui.center(board[lowestProbIndex].tuple), button='left')
         lowestProb = 100
         board[lowestProbIndex].isClicked = True
+        changeNeighbors(board, i, boardInfo)
         print("Clicked the " + str(lowestProbIndex) + " box, it had probability " + str(board[lowestProbIndex].probability))
         
     if findCoordinates('XFace.png', scale, 1) != None:
@@ -82,6 +115,7 @@ def main():
     numboxeswide = int(round((totalWidth-box[2])/box[2] + 1))
     numboxestall = int(round((totalHeight-box[3])/box[3] + 1))
     numboxes = numboxeswide*numboxestall
+    boardInfo = [numboxes,numboxeswide, numboxestall]
     xCoord = box[0]
     yCoord = box[1]
     board = []
@@ -99,7 +133,7 @@ def main():
             # print("We couldn't find O Face")
             # break
 
-    playGame(board, scale, numboxes)
+    playGame(board, scale, boardInfo)
 
 if __name__ == '__main__':
     main()
